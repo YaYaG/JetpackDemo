93a01d75ccc9cd15756d8b90fe5eb2f47b0e6e07
jack <jackwang@abdstem.com>
2020-01-23 21:45:14 +0800

    添加toolbar相关控件功能

--- navigation/src/main/java/com/wangjy/jetpackdemo/MainActivity.java
+++ navigation/src/main/java/com/wangjy/jetpackdemo/MainActivity.java
@@ -4,19 +4,25 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
 
     private NavController mNavController;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.toolbar);
         mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(mToolbar,mNavController);
     }
 
     public void goMessage(){
